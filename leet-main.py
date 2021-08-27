4b7f5295fa353fd44c3d6162de4fcbb1d0f5f790
rusnuker <49472785+rusnuker@users.noreply.github.com>
2021-08-27 21:12:46 +0200

    Add files via upload

---
+++ leet-main.py
@@ -0,0 +1,41 @@
+from argparse import ArgumentParser
+def main():
+	def simple(string):
+		leet_str = string.replace('e','3').replace('o','0')
+		return leet_str
+
+	def good(string):
+		leet_str = string.replace('l','1').replace('e','3').replace('o','0').replace('a','4')
+		return leet_str
+
+	def hard(string):
+		leet_str = string.replace('l','1').replace('e','3').replace('o','0').replace('t','7').replace('a','4').replace('g','6')
+		return leet_str
+	parser = ArgumentParser()
+	parser.add_argument("-o", "--output",
+		dest="filename", help="write translated string to FILE", metavar="FILE")
+	parser.add_argument("-t","--type", type=str,help="type for translation. can be \"simple\", \"good\" or \"hard\"", default="good")
+	parser.add_argument("string", type=str,help="string to translate.", nargs="?", )
+	args = parser.parse_args()
+	if args.filename:
+		with open(args.filename,"w") as f:
+			if args.type=="simple":
+				f.write(simple(args.string))
+			elif args.type=="good":
+				f.write(good(args.string))
+			elif args.type=="hard":
+				f.write(hard(args.string))
+			else:
+				raise "hell you, check help banner"
+		f.close()
+	else:
+		if args.type=="simple":
+			print(simple(args.string))
+		elif args.type=="good":
+			print(good(args.string))
+		elif args.type=="hard":
+			print(hard(args.string))
+		else:
+			raise "hell you, check help banner"
+if __name__=="__main__":
+	main()
